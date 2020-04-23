ae2a1e1190b7721b887e0a38f59a6ffb429fa860
Melody <leclecticat@gmail.com>
2020-04-23 15:16:55 -0700

    adding post component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import Post from 'Post.js'
 
 class Profile extends Component {
-    let posts = props.posts.map((post) => {
-        return (
-          <Post
-            key={postMessage._id}
-            title={title}
-          />
-        );
-      });
+  
     
     render() {
+        let posts = this.props.posts.map((post) => {
+            return (
+              <Post
+                key={postMessage._id}
+                // title={title}
+              />
+            );
+          });
+
         return (
             <div>
                 Profile page
