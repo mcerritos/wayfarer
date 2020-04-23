2245239ad47eb35d132f9a178f2e5096a037cffc
Melody <leclecticat@gmail.com>
2020-04-23 15:03:14 -0700

    adding post headlines to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class Profile extends Component {
+    let posts = props.posts.map((post) => {
+        return (
+          <Post
+            key={postMessage._id}
+            title={title}
+          />
+        );
+      });
+    
     render() {
         return (
             <div>
